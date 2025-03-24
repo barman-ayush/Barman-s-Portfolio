9e7b6a950cecb98fb65c726c115853f3d7d3ff52
Ayush <barmanayush2980@gmail.com>
2025-03-25 00:27:08 +0530

    Updated About section UI

--- src/Components/Landing/About/About.jsx
+++ src/Components/Landing/About/About.jsx
@@ -1,19 +1,18 @@
 import { Fragment } from "react";
 import BTN from "../../Button/Button.jsx";
-import Ayush from "../../../assets/about/about3.png";
-import "./About.css";
+import Ayush from "../../../assets/about/ayush.png";
 
 const About = ({ isBtn = false }) => {
   return (
     <Fragment>
-      <div className="flex text-white flex-wrap">
-        <div className="left-cont">
-          <p style={{color : "rgb(171, 178, 191)"}}  className=" py-3">
+      <div className="flex text-white flex-wrap flex-col md:flex-row">
+        <div className="left-cont w-full md:w-1/2 md:pr-8 md:px-0 px-4">
+          <p style={{color : "rgb(171, 178, 191)"}} className="py-3">
             I'm a pre-final year undergraduate at Indian Institiute Of
             Technology, Dhanbad pursuing Bachelor Of Technology in Computer
             Science And Engineering.
           </p>
-          <p style={{color : "rgb(171, 178, 191)"}}  className="">
+          <p style={{color : "rgb(171, 178, 191)"}}>
             I can develop responsive websites from scratch and raise them into
             modern user-friendly web experiences. Transforming my creativity and
             knowledge into a website has been my passion for over a year. I
@@ -30,13 +29,13 @@ const About = ({ isBtn = false }) => {
             <></>
           )}
         </div>
-        <div className="right-cont">
+        <div className="right-cont relative w-full md:w-1/2 flex justify-center md:justify-end mt-6 md:mt-0" style={{height : "30vh"}}>
           <img
             src={Ayush}
             height={507}
             width={339}
-            className="relative about-image"
-            style={{ borderBottom: "2px solid #C778DD" }}
+            className="about-image absolute"
+            style={{ borderBottom: "2px solid #C778DD" , top : "-50%" }}
           />
         </div>
       </div>
